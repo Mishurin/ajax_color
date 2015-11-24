8aa9f77ddeb4a09ae27f9a513780dd9daa37fef4
Igor Mishurin <borlandpascal@mail.ru>
2015-11-24 23:53:10 +0300

    Removed old code

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -2,41 +2,6 @@ var APP  = (function (app) {
 
     app.ajax = app.ajax || {};
 
-    var Request = function (params, cb, errb) {
-        this.params = params;
-        this.cb = cb;
-        this.errb = errb;
-    };
-
-    Request.prototype.send = function () {
-        var worker = new Worker('javascripts/delay.js'),
-            _this = this;
-        worker.addEventListener('message', function (e) {
-            switch (e.data) {
-                case 'READY':
-                    console.log('Loading...');
-                    break;
-                case 'COMPLETED':
-                    console.log('OK');
-                    _this.cb({
-                        payload: {
-                            color: _this.params.color,
-                            size: _this.params.width + ':' + _this.params.height
-                        }
-                    });
-                    worker.terminate();
-                    break;
-                case 'ERROR':
-                    console.log('ERROR');
-                    _this.errb(e);
-                    break;
-                default:
-                    console.log('UNCOVERED CASE');
-            }
-        });
-        return this;
-    };
-
     app.ajax.post = function (params, cb, errb) {
         var xhr = new XMLHttpRequest();
         xhr.open('POST', app.config.ECHO_SERVER_REMOTE_URL, true);
--- public/javascripts/delay.js
+++
@@ -1,7 +0,0 @@
-var randomDelay = Math.floor(Math.random()* 3 + 1000);
-self.postMessage('READY');
-var timer = setTimeout(function () {
-    self.postMessage('COMPLETED');
-    timer.clearTimeout();
-}, randomDelay);
-
